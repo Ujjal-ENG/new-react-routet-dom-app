10a69e353d2a6d2a3518d073c5dab41fac5f0558
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-03 16:57:21 +0600

    everything is work properly

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./components/pages/About";
-import ContactUs from "./components/pages/ContactUs";
-import Users from "./components/pages/Users";
-import UserDetails from "./components/pages/UserDetails";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact-us",
-        element: <ContactUs />,
-      },
-      {
-        path: "users",
-        element: <Users />,
-        loader: async () => fetch("https://jsonplaceholder.typicode.com/users"),
-      },
-      {
-        path: "user-details/:id",
-        element: <UserDetails />,
-        loader: async ({ params }) =>
-          fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`),
-      },
-    ],
-  },
-]);
+import { router } from "./components/routes/Routes.app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
